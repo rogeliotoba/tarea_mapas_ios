f3e1e672ea6e36afdd3da7154123a5d68cd38e77
Rogelio Torres <rogeliotoba@gmail.com>
2014-03-27 10:53:22 -0600

    Initial Commit

---
+++ tarea_mapas_ios/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  tarea_mapas_ios
+//
+//  Created by Rogelio Torres on 27/03/14.
+//  Copyright (c) 2014 Rogelio Torres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ tarea_mapas_iosTests/tarea_mapas_iosTests.m
@@ -0,0 +1,34 @@
+//
+//  tarea_mapas_iosTests.m
+//  tarea_mapas_iosTests
+//
+//  Created by Rogelio Torres on 27/03/14.
+//  Copyright (c) 2014 Rogelio Torres. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface tarea_mapas_iosTests : XCTestCase
+
+@end
+
+@implementation tarea_mapas_iosTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
